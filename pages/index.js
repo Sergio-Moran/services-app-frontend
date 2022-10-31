e0c089bb341ec20e16f490deee7bc410bdfda0c5
Sergio Moran <sergioMoran@gmail.com>
2022-10-31 17:39:40 -0600

    First commint

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+import Register from "../components/Register";
+
+export default function Home() {
+  return <Register />;
+}
