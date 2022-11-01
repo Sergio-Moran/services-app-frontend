a722ae516a37195a311789f29198d778d90a44e2
Sergio Moran <sergioMoran@gmail.com>
2022-11-01 15:19:43 -0600

    Adding styles from primereact

--- pages/LogPage.js
+++ pages/LogPage.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import Register from "../components/Register";
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
 
 const LogPage = () => {
   return (
