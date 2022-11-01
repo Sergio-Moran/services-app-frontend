901e9bda69d1afe33bdca82df12246b14561cd59
JhessReyes <jhessaureyes@gmail.com>
2022-11-01 13:32:44 -0600

    conection to back

--- routes/api.routes.js
+++ routes/api.routes.js
@@ -1,22 +1,24 @@
-const ROUTE = "https://services-app-backend.vercel.app";
+const ROUTE = "https://services-app-backend.vercel.app/";
 
 export const getUsers = async () => {
   const response = await fetch(`${ROUTE}/getUsers`, {
     method: "GET",
+    credentials: "include",
   });
   return await response.json();
 };
 
 export const validateUser = async (userCredentials) => {
-  console.log(userCredentials)
+  console.log(userCredentials);
   const response = await fetch(`${ROUTE}/validateUser`, {
     method: "POST",
     mode: "cors",
+    credentials: "same-origin",
     headers: {
       Accept: "application/json",
       "Content-Type": "application/json",
     },
     body: JSON.stringify(userCredentials),
   });
-  return await response;
+  return await response.json();
 };
