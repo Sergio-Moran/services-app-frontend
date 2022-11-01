eb702bf6e57d0fd7d5fa3bf4cd82dfbe66d5bc04
Sergio Moran <sergioMoran@gmail.com>
2022-10-31 18:36:35 -0600

    Adding file route

---
+++ routes/api.routes.js
@@ -0,0 +1,21 @@
+const ROUTE = "https://services-app-backend.vercel.app";
+
+export const getUsers = async () => {
+  const response = await fetch(`${ROUTE}/getUsers`, {
+    method: "GET",
+  });
+  return await response.json();
+};
+
+export const validateUser = async (userCredentials) => {
+  const response = await fetch(`${ROUTE}/validateUser`, {
+    method: "POST",
+    mode: "cors",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(userCredentials),
+  });
+  return await response.json();
+};
