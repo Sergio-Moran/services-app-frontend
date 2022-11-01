0cc571ad152dd78532cf590129678f3079061e29
Sergio Moran <sergioMoran@gmail.com>
2022-11-01 15:27:10 -0600

    Variable

--- routes/api.routes.js
+++ routes/api.routes.js
@@ -1,5 +1,5 @@
-/* const ROUTE = "https://services-app-backend.vercel.app"; */
-const ROUTE = "http://localhost:3002";
+const ROUTE = "https://services-app-backend.vercel.app";
+/* const ROUTE = "http://localhost:3002"; */
 
 export const getUsers = async () => {
   const response = await fetch(`${ROUTE}/getUsers`, {
