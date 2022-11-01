4319001d5bceaab81723e3400ba3272ee6d841a0
Sergio Moran <sergioMoran@gmail.com>
2022-11-01 17:31:29 -0600

    form pay

---
+++ components/FormPayServices.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import { InputText } from "primereact/inputtext";
+import { InputNumber } from "primereact/inputnumber";
+import { Card } from "primereact/card";
+
+const FormPayServices = () => {
+  return (
+    <div
+      className="card"
+      style={{
+        height: "100vh",
+      }}
+    >
+      <Card>
+        <h5>Addons</h5>
+        <div className="grid p-fluid">
+          <div className="col-12 md:col-4">
+            <div className="p-inputgroup">
+              <span className="p-inputgroup-addon">
+                <i className="pi pi-user"></i>
+              </span>
+              <InputText placeholder="Username" />
+            </div>
+          </div>
+
+          <div className="col-12 md:col-4">
+            <div className="p-inputgroup">
+              <span className="p-inputgroup-addon">$</span>
+              <InputNumber placeholder="Price" />
+              <span className="p-inputgroup-addon">.00</span>
+            </div>
+          </div>
+
+          <div className="col-12 md:col-4">
+            <div className="p-inputgroup">
+              <span className="p-inputgroup-addon">www</span>
+              <InputText placeholder="Website" />
+            </div>
+          </div>
+        </div>
+      </Card>
+    </div>
+  );
+};
+
+export default FormPayServices;
--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,10 @@ import "primeicons/primeicons.css";
 import Login from "../components/Login";
 import Table from "../components/Table";
 import List from "../components/List";
+import FormPayServices from "../components/FormPayServices";
 
 export default function Home() {
   return (
-    <Login />
+    <FormPayServices />
   )
 }
