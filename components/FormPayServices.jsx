976dabfd50d2873f573bb08c7ab490e973ffa7f1
Sergio Moran <sergioMoran@gmail.com>
2022-11-01 18:27:32 -0600

    Adding form payment

--- components/FormPayServices.jsx
+++ components/FormPayServices.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { InputText } from "primereact/inputtext";
 import { InputNumber } from "primereact/inputnumber";
-import { Card } from "primereact/card";
+import { Button } from "primereact/button";
 
 const FormPayServices = () => {
   return (
@@ -11,32 +11,41 @@ const FormPayServices = () => {
         height: "100vh",
       }}
     >
-        <h5>Addons</h5>
-        <div className="grid p-fluid">
-          <div className="col-12 md:col-4">
-            <div className="p-inputgroup">
-              <span className="p-inputgroup-addon">
-                <i className="pi pi-user"></i>
-              </span>
-              <InputText placeholder="Username" />
-            </div>
-          </div>
-
-          <div className="col-12 md:col-4">
-            <div className="p-inputgroup">
-              <span className="p-inputgroup-addon">$</span>
-              <InputNumber placeholder="Price" />
-              <span className="p-inputgroup-addon">.00</span>
-            </div>
-          </div>
-
-          <div className="col-12 md:col-4">
-            <div className="p-inputgroup">
-              <span className="p-inputgroup-addon">www</span>
-              <InputText placeholder="Website" />
-            </div>
+      <h5>Pay</h5>
+      <div className="grid p-fluid">
+        <div className="col-12 md:col-4">
+          <div className="p-inputgroup">
+            <span className="p-inputgroup-addon">
+              <i className="pi pi-user"></i>
+            </span>
+            <InputText placeholder="Username" />
           </div>
         </div>
+
+        <div className="col-12 md:col-4">
+          <div className="p-inputgroup">
+            <span className="p-inputgroup-addon">$</span>
+            <InputNumber placeholder="Price" />
+            <span className="p-inputgroup-addon">.00</span>
+          </div>
+        </div>
+
+        <div className="col-12 md:col-4">
+          <div className="p-inputgroup">
+            <span className="p-inputgroup-addon">www</span>
+            <InputText placeholder="Website" />
+          </div>
+        </div>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Button label="Success" className="p-button-rounded p-button-success" />
+      </div>
     </div>
   );
 };
